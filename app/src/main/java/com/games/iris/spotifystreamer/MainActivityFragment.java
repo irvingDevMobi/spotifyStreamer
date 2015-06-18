4fd855a29a551f00f0bb43fb6e888737ad1d9430
Irving Lóp <irving.lopez@unam.mobi>
2015-06-18 09:55:06 -0500

    UI added

--- app/src/main/java/com/games/iris/spotifystreamer/MainActivityFragment.java
+++ app/src/main/java/com/games/iris/spotifystreamer/MainActivityFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 
 /**
@@ -18,6 +19,11 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        EditText editText = (EditText) rootView.findViewById(R.id.input_main_editText);
+
+        return rootView;
     }
 }
